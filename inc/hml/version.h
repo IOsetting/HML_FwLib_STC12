38e8d84cd72efefd68775cb490c657272f273814
WeilunFong <wlf@zhishan-iot.tk>
2020-01-25 11:58:50 +0800

    refactor: refactor hml/version.h

--- inc/hml/version.h
+++ inc/hml/version.h
@@ -16,13 +16,54 @@
  *****************************************************************************/
 
 /**
- *\brief: version code
+ *\brief: string literal representation of the current library version
  */
-#define HML_VERSION_CODE "0.0.0"
+#define __HML_VERSION_STRING__ "0.0.1"
+
+/**
+ *\ @deprecated: these macro will be abolished in next minor version
+ */
+#define HML_VERSION_CODE "0.0.1"
 #define HML_VERSION_NAME "HML_FwLib_STC11"
 
 /**
- *\brief: secondary version code
+ *\ brief:
+ * Numerical representation of the current library version.
+ *
+ * In the numerical representation, the major number is multiplied by
+ * 10000, the minor number by 100, and all three parts are then
+ * added.  It is intented to provide a monotonically increasing
+ * numerical value that can easily be used in numerical checks.
+ */
+#define __HML_VERSION__ 000001UL
+
+/**
+ *\breif: string literal representation of the release date.
+ */
+#define __HML_DATE_STRING__ "20200131"
+
+/**
+ *\breif: numerical representation of the release date.
+ */
+#define __HML_DATE__ 20200131UL
+
+/**
+ *\breif: library major version number.
+ */
+#define __HML_VERSION_MAJOR__ 0
+
+/**
+ *\brief: library minor version number.
+ */
+#define __HML_VERSION_MINOR__ 0
+
+/**
+ *\brief: library patch version number.
+ */
+#define __HML_VERSION_PATCH__ 1
+
+/**
+ *\ @deprecated: following macro will be abolished in next minor version
  */
 #ifdef HML_VERSION_MAJOR
     #undef HML_VERSION_MAJOR
@@ -37,6 +78,6 @@
 #ifdef HML_VERSION_PATCH
     #undef HML_VERSION_PATCH
 #endif
-#define HML_VERSION_PATCH 0
+#define HML_VERSION_PATCH 1
 
 #endif
